9462e893e0f78f22758044c603e13193d8b39118
Dragan Radovac <dragan.radovac.75@gmail.com>
2025-01-07 05:46:50 +1100

    Update to main

---
+++ application.cs
@@ -0,0 +1,13 @@
+﻿namespace App;
+
+public partial class Application : System.Windows.Application
+{
+    [System.STAThread()]
+    static void Main()
+    {
+        var application = new Application() { MainWindow = new View.Application() };        
+        LoadComponent(application.MainWindow, new System.Uri("/application;component/application.xaml", System.UriKind.Relative));
+        application.MainWindow.Show();
+        application.Run();
+    }
+}
